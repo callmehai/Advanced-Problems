055806ef23d2723d4fcfebb0b3b230e4d1c1757f
callmehai <108572472+callmehai@users.noreply.github.com>
2023-11-20 19:18:02 +0700

    tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -1,7 +1,87 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#include <cstring>
+#endif
+
+#if __cplusplus >= 202002L
+#include <barrier>
+#include <bit>
+#include <compare>
+#include <concepts>
+#if __cpp_impl_coroutine
+# include <coroutine>
+#endif
+#include <latch>
+#include <numbers>
+#include <span>
+#include <semaphore>
+#include <version>
+#endif
+
+#if __cplusplus > 202002L
+#include <spanstream>
+#endif
+// #include<bits/stdc++.h>
 using namespace std;
-#define FPT_5G_8$_per_month ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define ll long long 
+// ************ start coding ************
+#define FPTU ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
 #define pii pair<ll, ll >
 #define pip pair<ll, pair<ll,pii> >
 #define oo 1000111000
@@ -9,7 +89,6 @@ using namespace std;
 #define fi first
 #define se second
 vector<pii > WASD={{-1,0}, {0,-1}, {0,1}, {1,0}};
-// moi moi moi moi moi moi moi moi moi moi
 const int N=25;
 ll n, m;
 pii b[N];
@@ -17,8 +96,9 @@ bool vis[N][N];
 // =========> <3 Han Han <3 <=======
 int main()
 {
-    FPT_5G_8$_per_month; // overpriced
-    //int Test=1;         //cin>>Test;
+    FPTU;
+    freopen("inp.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
     
     return 0;
 }
