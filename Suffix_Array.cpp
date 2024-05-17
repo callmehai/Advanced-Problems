5d853157e28146800ac913e21b1c1e21490a8f45
callmehai <108572472+callmehai@users.noreply.github.com>
2024-05-17 21:33:15 +0700

    Create Suffix_Array.cpp

---
+++ Suffix_Array.cpp
@@ -0,0 +1,194 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#include <cstring>
+#endif
+
+#if __cplusplus >= 202002L
+#include <barrier>
+#include <bit>
+#include <compare>
+#include <concepts>
+#if __cpp_impl_coroutine
+# include <coroutine>
+#endif
+#include <latch>
+#include <numbers>
+#include <span>
+#include <semaphore>
+#include <version>
+#endif
+
+#if __cplusplus > 202002L
+#include <spanstream>
+#endif
+using namespace std;
+
+// ************ Setting up  ************
+#define FPTU ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define TIME  (1.0 * clock() / CLOCKS_PER_SEC)
+#define BUG(x) {cerr << #x << " = " << x << endl;}
+#define pii pair<int,int>
+#define pip pair<int,pii>
+#define ppi pair<pii,int>
+#define ll long long
+#define ull unsigned long long
+#define usi unsigned int
+#define pll pair<ll,ll>
+#define plp pair<ll,pll>
+#define ppl pair<pll,ll>
+#define ppp pair<pii,pii>
+#define oo 1000111000
+#define ooo 1000111000111000111
+#define fi first
+#define se second
+#define pri_qu priority_queue
+#define vt vector
+#define pb push_back
+#define all(arr) arr.begin(),arr.end()
+void build_test();
+random_device rd;
+mt19937 mt(rd());
+ll rand_num(ll a,ll b)
+{
+    uniform_int_distribution<ll> dist(a,b);
+    return dist(mt);
+}
+template <typename T> void minimize(T &a, T b){ if(a>b) a=b;}
+template <typename T> void maximize(T &a, T b){ if(a<b) a=b;}
+template <typename T> T gcd(T a, T b)
+{
+    while(a!=0&&b!=0)
+    {
+        if(a>b) a%=b;
+        else b%=a;
+    }
+    return a+b;
+}
+bool bit(ll a,int i) { return a>>i&1; }
+vt<pii> wasd={{-1,0}, {0,-1}, {0,1}, {1,0}};
+vt<pii> WASD={{-1,0}, {0,-1}, {0,1}, {1,0}, {-1,-1}, {-1,1}, {1,-1}, {1,1}};
+vt<pii> knight={{-1,-1},{-1,1},{1,-1},{1,1}};
+vt<string> step ={"U", "L", "R", "D", "LU", "RU", "LD", "RD"};
+pri_qu<pii,vector<pii>,greater<pii>> pq;
+
+// =========> <3 VietHai1709 <3  <=======
+const int N=1e6+5;
+
+void radix_sort(vector<ppi> &arr) {
+    for(int i=1;i<=2;i++)
+    {
+        auto key = [&](const ppi x)
+        {
+            return i==1 ? x.fi.se : x.fi.fi;
+        };
+        int MAX=0;
+        for(auto &x:arr) maximize(MAX,key(x));
+        vt<int> cnt(MAX+1,0);
+        for(auto &x:arr) cnt[key(x)]++;
+        vt<int> start(MAX+1);
+        for(int j=1;j<=MAX;j++) start[j]=start[j-1]+cnt[j-1];
+        vt<ppi> new_arr(arr.size());
+        for(auto &x:arr)
+        {
+            new_arr[start[key(x)]]=x;
+            start[(key(x))]++;
+        }
+        arr=new_arr;
+    }
+       
+}
+
+int main(){
+    FPTU;
+    freopen("inp.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    string s;
+    cin >> s;
+    s += '$';
+    const int n= (int) s.size();
+    vt<ppi> suffs(n);
+    for(int i=0;i<n;i++) suffs[i]={{s[i],s[i]},i};
+    sort(all(suffs));
+    vt<int> ranks(n);
+    for(int i=1;i<n;i++)
+    {
+        ppi c = suffs[i];
+        ppi p = suffs[i-1];
+        ranks[c.se] = ranks[p.se] + (c.fi>p.fi);
+    }
+    for(int k=1;k<n;k*=2)
+    {
+        for(auto &[val,id]:suffs)
+            val={ranks[id],ranks[(id+k)%n]};
+        
+        radix_sort(suffs);
+        
+        for(int i=1;i<n;i++)
+        {
+            ppi c = suffs[i];
+            ppi p = suffs[i-1];
+            ranks[c.se] = ranks[p.se] + (c.fi>p.fi);
+        }
+    }
+    for (int i = 1; i < n; i++) {
+        cout << suffs[i].second << " \n"[i == n - 1];
+    }
+    cerr << "Time elapsed: " << TIME << " s.\n";
+    return 0;
+}
